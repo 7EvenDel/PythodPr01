1d55143d82dd8f1adacdd118951fde1002315018
7EvenDel <116507218+7EvenDel@users.noreply.github.com>
2024-02-28 10:50:05 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+var_int = 10
+var_float = 8.4
+var_str = "No"
+print(var_int,var_float,var_str)
+
+big_int = var_int * 3.5
+print(big_int)
+
+var_float -= 1
+print(var_float)
+
+var_int / var_float
+big_int / var_float
+
+var_str = "No" * 2 + "Yes" * 3
+print(var_str)
+
+a = 2
+x = 6
+y = 5
+z = 1
+
+result = (1+z)*((x+y/x)/(a-(1/(1+x))))
+print(result)
